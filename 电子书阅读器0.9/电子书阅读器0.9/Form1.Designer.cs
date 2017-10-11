36cec57beaf916d640c4b8ce379412f9b5d137bf
EVE486 <674702685@qq.com>
2017-10-11 19:58:07 +0800

    新推送的电子书阅读器0.9 缺陷：在翻页调度最前页和最后一页时候出现问题

---
+++ 电子书阅读器0.9/电子书阅读器0.9/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace 电子书阅读器0._9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 516);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 25);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "下一页";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 516);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 25);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "上一页";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(445, 516);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(93, 25);
+            this.numericUpDown1.TabIndex = 10;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(12, 57);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(135, 454);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 38);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "添加书本";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 516);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(135, 25);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = "该书页数：";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(153, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(385, 497);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 563);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ 电子书阅读器0.9/电子书阅读器0.9/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace 电子书阅读器0._9
+{
+    public partial class Form1 : Form
+    {
+        
+        static string[] t = { "资本论（节选）.txt", "资本论（节选2）.txt", "资本论（节选3）.txt", "", "", "", "", "", "", "", "", "", "", "", "" };
+        int hum=0;
+        int start;
+        int i = 3;
+        int m;
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Add("资本论（节选）.txt");
+            listBox1.Items.Add("资本论（节选2）.txt");
+            listBox1.Items.Add("资本论（节选3）.txt");
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 0;
+            //将列表listbox中选定项的索引加载到richTextBox1中
+            if (listBox1.SelectedIndex >= 0)
+                richTextBox1.LoadFile(t[this.listBox1.SelectedIndex], RichTextBoxStreamType.PlainText);
+            m = richTextBox1.TextLength / 550;
+            textBox2.Text = "该书页数："+m;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) //添加书本
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.InitialDirectory = Application.StartupPath;  //默认目录为exe运行文件所在的文件夹
+            fileDialog.Filter = "All files(*.*)|*.*|txt files(*.txt)|*.txt";  //设置控件打开的文件类型
+            fileDialog.FilterIndex = 2;
+            fileDialog.RestoreDirectory = true;
+           
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                t[i] = fileDialog.FileName;
+                listBox1.Items.Add(fileDialog.FileName);  
+                i++;
+                richTextBox1.LoadFile(fileDialog.FileName, RichTextBoxStreamType.PlainText);
+
+            }
+            
+        }
+
+  
+         
+        private void button1_Click(object sender, EventArgs e)//上翻
+        {
+          if (listBox1.SelectedIndex >= 0 && listBox1.SelectedIndex < t.Length)
+                if (File.Exists(t[listBox1.SelectedIndex]))
+                {
+                    if (hum <= 0)
+                    {
+                        this.richTextBox1.Select(richTextBox1.Text.Length,richTextBox1.Text.Length);
+                        this.richTextBox1.ScrollToCaret();
+                        
+                    }
+                    else
+                    {
+                        hum = hum - 25;
+                        start = this.richTextBox1.GetFirstCharIndexFromLine(hum);
+                        this.richTextBox1.SelectionStart = start;
+                        this.richTextBox1.ScrollToCaret();
+                    }
+                }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//下翻
+        {
+            if (listBox1.SelectedIndex >= 0 && listBox1.SelectedIndex < t.Length)
+                if (File.Exists(t[listBox1.SelectedIndex]))
+            {
+
+                int x1;
+                x1 = richTextBox1.Text.Length/22;
+                if (hum > x1)
+                {
+                    this.richTextBox1.Select(richTextBox1.Text.Length, 0);
+                    this.richTextBox1.ScrollToCaret();
+                    
+                }
+                else
+                {
+                    hum = hum + 25;
+                    start = this.richTextBox1.GetFirstCharIndexFromLine(hum);
+                    this.richTextBox1.SelectionStart = start;
+                    this.richTextBox1.ScrollToCaret();
+                    
+                }
+                // this.richTextBox1.Select(richTextBox1.Text.Length, 0);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Maximum = m;
+            numericUpDown1.Minimum = 0;
+            StreamReader sr =null;
+            string text = null;
+            if(listBox1.SelectedIndex>=0 && listBox1.SelectedIndex<t.Length)
+                if(File.Exists(t[listBox1.SelectedIndex]))
+                {
+                    sr=new StreamReader(t[listBox1.SelectedIndex],System.Text.Encoding.Default);
+                    text=sr.ReadToEnd();
+                    sr.Close();
+                }
+            if(listBox1.SelectedIndex!=-1)
+            {
+              if((int)numericUpDown1.Value<m && (int)numericUpDown1.Value>0)
+              {
+                  int gom = (int)numericUpDown1.Value * 25;
+                  start = this.richTextBox1.GetFirstCharIndexFromLine(gom);
+                  this.richTextBox1.SelectionStart = start;
+                  this.richTextBox1.ScrollToCaret();
+                  
+              }
+            }
+        }
+         
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e) //当前页数
+        {
+           
+        }
+
+       /* class Book
+    {
+        private int length;
+        private string text;
+            
+        public void setLength(int length)
+        {
+            this.length = length;
+        }
+        public int getLength() 
+        {
+            return length;
+        }
+        public void setText(string text)
+        {
+            this.text = text;
+        }
+        public string getText()
+        {
+            return text;
+        }
+        public string getPage(int pageNumber,int a) //翻页  
+        {
+            if (length > 0)
+            {
+                // 总页数=1+【（总字数-不足一页的字数）】/每页字数
+                int totalPages = ((length - text.Length % length) + text.Length) / length;
+                //页码>0、页码<总页数、总页数>0
+                if (pageNumber > 0 && pageNumber <= totalPages && totalPages > 0)
+                {
+                    // 跳至指定页，需要跳过多少字为pageTop
+                    int pageTop = length * (pageNumber - 1);
+                    //如果阅读页没有超过总字数，返回输入页数，否则返回
+                    if (pageTop + length < text.Length)
+                        return text.Substring(pageTop,a);
+                    else
+                        return text.Substring(pageTop, text.Length - pageTop);
+                }
+            }
+                return null;
+        }
+
+      
+    }
+        */
+    }
+}
+
+    
+
